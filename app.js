e58faf305494565634f2aec4554943c5ba81e458
Norbert <kurzawski.n@gmail.com>
2020-12-05 17:38:14 +0100

    added a functions that when clicked is showing us our choice

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('Hello');
+// const appState = {
+//     userName: '',
+//     selectedItem: '',
+//     computerSelection: '',
+//     round ,
+//     points: [user, computer]
+// };
+
+const paper = document.getElementById('paper');
+const rock = document.getElementById('rock');
+const scissors = document.getElementById('scissors');
+
+const choosenPaper = document.getElementById('choosen-paper');
+const choosenRock = document.getElementById('choosen-rock');
+const choosenScissors = document.getElementById('choosen-scissors');
+
+paper.addEventListener('click', () => {
+    choosenRock.classList.remove('item-visible');
+    choosenScissors.classList.remove('item-visible');
+    choosenPaper.classList.add('item-visible');
+})
+
+rock.addEventListener('click', () => {
+    choosenPaper.classList.remove('item-visible');
+    choosenScissors.classList.remove('item-visible');
+    choosenRock.classList.add('item-visible');
+})
+
+scissors.addEventListener('click', () => {
+    choosenPaper.classList.remove('item-visible');
+    choosenRock.classList.remove('item-visible');
+    choosenScissors.classList.add('item-visible');
+})
