badcf496a56c234b3eda3e4db84c27dcc9c9a27c
Norbert <kurzawski.n@gmail.com>
2020-12-12 16:52:43 +0100

    Created modal which transmits user's name to paragraph created for it. Added button which is converting data in appState.

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-// const appState = {
-//     userName,
-//     userSelectedItem,
-//     computerSelectedItem,
-//     round,
-//     points: [user, computer]
-// };
+ const appState = {
+    userName: '',
+    userSelectedItem: '',
+    computerSelectedItem: '',
+    round: 1,
+    points: [
+        {user: 0},
+        {computer: 0}
+    ]
+};
 
-console.log('hello world');
 
+// Modal getting elements
+const modal = document.getElementById('modal');
+const submitNameBtn = document.getElementById('submit-name-btn');
+const inputName = document.getElementById('first-user-name');
+
+// game getting elements
 const paper = document.getElementById('paper');
 const rock = document.getElementById('rock');
 const scissors = document.getElementById('scissors');
@@ -16,20 +24,54 @@ const choosenPaper = document.getElementById('choosen-paper');
 const choosenRock = document.getElementById('choosen-rock');
 const choosenScissors = document.getElementById('choosen-scissors');
 
+const submitButton = document.getElementById('submit-button');
+
+const givenName = document.getElementById('user-name');
+
+
+// Modal running
+submitNameBtn.addEventListener('click', () => {
+    appState.userName = inputName.value;
+    givenName.innerHTML = appState.userName;
+
+    modal.classList.add('invisible');
+})
+
+// After modal
+function showButton() {
+    submitButton.classList.add('show-button');
+}
+
+function startPlaying() {
+    // Computer's turn
+}
+
 paper.addEventListener('click', () => {
+    appState.userSelectedItem = 'Paper';
+
     choosenRock.classList.remove('item-visible');
     choosenScissors.classList.remove('item-visible');
     choosenPaper.classList.add('item-visible');
+
+    showButton();
 })
 
 rock.addEventListener('click', () => {
+    appState.userSelectedItem = 'Rock';
+
     choosenPaper.classList.remove('item-visible');
     choosenScissors.classList.remove('item-visible');
     choosenRock.classList.add('item-visible');
+
+    showButton();
 })
 
 scissors.addEventListener('click', () => {
+    appState.userSelectedItem = 'Scissors';
+
     choosenPaper.classList.remove('item-visible');
     choosenRock.classList.remove('item-visible');
     choosenScissors.classList.add('item-visible');
+
+    showButton();
 })
